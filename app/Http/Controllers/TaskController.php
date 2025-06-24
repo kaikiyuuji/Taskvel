181c693be3584a10883f4c9c54bed44135a7026a
kaikiyuuji <kaikiramoshirata@gmail.com>
2025-06-24 11:48:28 -0300

    Método index de tarefas

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Task;
+use App\Models\TaskList;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class TaskController extends Controller
 {
@@ -12,7 +15,41 @@ class TaskController extends Controller
      */
     public function index()
     {
+        $query = Task::with('list')
+            ->whereHas('list', function ($query) {
+                $query->where('user_id', auth()->id());
+            })->orderBy('created_at', 'desc');
 
+        if ($request->has('search'))
+        {
+            $search = $request->get('search');
+            $query->where(function ($q) use ($search)
+            {
+                $q->where('title', 'like', "%{$search}%")
+                ->orWhere('description', 'like', "%{$search}%");
+            });
+        }
+
+        if ($request->has('filter') && $request->get('filter') == 'all')
+        {
+            $query->where('is_completed', request('filter') === 'completed');
+        }
+
+        $tasks = $query->paginate(10);
+        $lists = TaskList::where('user_id', auth()->id())->get();
+        return Inertia::render('Tasks/Index', [
+            'tasks' => $tasks,
+            'lists' => $lists,
+            'filters' => [
+                'search' => request('search', ''),
+                'filter' => request('filter', ''),
+
+            ],
+            'flash' => [
+                'success' => session('success'),
+                'error' => session('error'),
+            ]
+            ]);
     }
 
     /**
